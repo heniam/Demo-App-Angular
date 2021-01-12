9c07968a62563ae1f6e0bdf5ca406f15a1192e1c
heniam <heniam2012@gmail.com>
2021-01-12 17:15:42 -0500

    Forms, Validation and insert
    Forms - ReactiveFormsModule

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
+  //the order is before id beacause the router
+  // will go to it first 
+  { path: 'insert', component: ProductInsertComponent },
   { path: ':id', component: ProductDetailComponent },
 ];
 
